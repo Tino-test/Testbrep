b4e231d558ea4af347241ad2a9370c704da43f9f
Tino-test <moraistrino@gmail.com>
2021-07-19 12:05:33 +0530

    Hello

---
+++ src/ab/Pgmodel.java
@@ -0,0 +1,13 @@
+package ab;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class Pgmodel {
+	@FindBy(id="email")
+	private WebElement email;
+	
+
+
+	
+}
